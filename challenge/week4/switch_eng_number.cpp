d7675ac404c8fd322b392aef7c2bd36ad038a7a1
tjdekdms <tjdekdms0608@naver.com>
2024-09-29 20:32:11 +0900

    If and switch Exercises

---
+++ challenge/week4/switch_eng_number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "숫자를 입력하시오:";
+    cin >> number;
+    switch (number)
+    {
+        case 0:
+            cout << "zero\n";
+            break;
+        case 1:
+            cout << "one\n";
+            break;
+        default:
+            cout << "many\n";
+            break;
+    }
+    
+}
